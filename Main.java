841f6dd0fc766c6b4c6da3719cfbf2a5e4efbcd9
Filip Tomczyk <70267957+fantom2k00@users.noreply.github.com>
2021-03-11 16:36:19 +0100

    Updated Code with GUI

--- Main.java
+++ Main.java
@@ -1,6 +1,28 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.print(Difficulty.BEGINNER);
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,200);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        JLabel lbl = new JLabel("Select Difficulty");
+        lbl.setVisible(true);
+        panel.add(lbl);
+
+        String[] choices = {"Beginner", "Intermediate", "Hard"};
+        final JComboBox<String> cb = new JComboBox<String>(choices);
+        cb.setVisible(true);
+        panel.add(cb);
+
+        JButton btn = new JButton("OK");
+        panel.add(btn);
+        frame.setVisible(true);
     }
+
+
 }
