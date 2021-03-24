73602df073f652b759d4cf8e943439e0020c5ac0
BenarjiSai19 <benarjisai@gmail.com>
2021-03-24 19:38:27 +0100

    Test12345

--- Lab 4-6/MineSweeper-master 2/Map.java
+++ Lab 4-6/MineSweeper-master 2/Map.java
@@ -1,19 +1,24 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.*;
 import java.awt.*;
 
-public class Map extends JFrame implements ActionListener {
+public class Map extends JFrame implements MouseListener{
 
 	private int boardSizeX;
 	private int boardSizeY;
 	private int mineCount;
+	private int track;
+	private int bombCount;
 	private ArrayList<Tile> tiles;
 	private ArrayList<Integer> mineLocations;
 	private ArrayList<JButton> buttons;
-//change 
 	private JFrame frame;
+	private JFrame endframe;
+
 
 	public Map(int sizeX, int sizeY, int mines) {
 		boardSizeX = sizeX;
@@ -22,19 +27,35 @@ public class Map extends JFrame implements ActionListener {
 		tiles = new ArrayList<Tile>();
 		mineLocations = new ArrayList<Integer>();
 		buttons = new ArrayList<JButton>();
+		this.mineGenerator();
+		track = 0;
+		bombCount = 0;
 
 		frame = new JFrame("Mine Sweeper");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(sizeX*30 + 25, sizeY * 30 + 50);
 		frame.setVisible(true);
 		frame.setLayout(null);
+		/*JLabel flags = new JLabel("Bombs left" + bombCount);
 
-		mineGenerator();
+		frame.add(flags, BorderLayout.NORTH);*/
+
+		/*endframe = new JFrame("RIP");
+		endframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		endframe.setSize(sizeX*30 + 25, sizeY * 30 + 50);
+		endframe.setVisible(false);
+		endframe.setLayout(null);*/
+
+
 
 		for(int i = 0; i < sizeX; i++) {
 			for(int j = 0; j < sizeY; j++) {
 
 				JButton tileButton = new JButton("");
+
+				tileButton.setOpaque(true);
+				tileButton.setBorderPainted(false);
+
 				Tile tile;
 
 				if(mineLocations.contains(i*boardSizeX + j)) {
@@ -49,7 +70,8 @@ public class Map extends JFrame implements ActionListener {
 				}
 
 				tileButton.setBounds(i*30 + 4, j*30, 30, 30);
-				tileButton.addActionListener(this);
+				tileButton.addMouseListener(this);
+
 
 				frame.add(tileButton);
 				buttons.add(tileButton);
@@ -57,7 +79,60 @@ public class Map extends JFrame implements ActionListener {
 			}
 		}
 	}
+	public void mineGenerator()
+	{
+		int random;
+		for(int i = 0; i < mineCount; i++) {
+			random = (int)(Math.random() * (boardSizeX * boardSizeY + 1));
+			while(mineLocations.contains(random)) {
+				random = (int)(Math.random() * (boardSizeX * boardSizeY + 1));
+			}
+			System.out.println(random);
+			mineLocations.add(random);
+		}
+	}
+	/*public void actionPerformed(ActionEvent e)
+	{
+		for (JButton btn : buttons) {
+			if (e.getSource() == btn) {
+				if(btn.getName().equals("Empty")) {
+					int nearbyMines = countNearbyMines(btn.getX()/30, btn.getY()/30);
+					if(nearbyMines > 0) {
+						JLabel mineText = new JLabel(Integer.toString(nearbyMines));
+						mineText.setBounds(btn.getBounds());
+						mineText.setVisible(true);
+						frame.add(mineText);
 
+					}
+					//else{
+					//open empties}
+					btn.setVisible(false);
+					break;
+				}
+
+				else {
+					for (JButton button : buttons) {
+						if(button.getName().equals("Mine")) {
+							for (JButton button1 : buttons){
+								if(button1.getName().equals("Mine")) {
+							button1.setVisible(false);}
+							}
+							JOptionPane.showMessageDialog(null, "R.I.P. YOU HAVE PRESSED THE BOMB", "Game over", JOptionPane.CLOSED_OPTION);
+
+							frame.setVisible(false);
+							break;
+							/*endframe.setVisible(true);
+							endframe.setLayout(null);
+							endframe.setLocation(500,500);
+
+
+						}
+					}
+					break;
+				}
+			}
+		}
+	}*/
 	public int getBoardSizeX() {
 		return this.boardSizeX;
 	}
@@ -82,18 +157,7 @@ public class Map extends JFrame implements ActionListener {
 		this.mineCount = mineCount;
 	}
 
-	public void mineGenerator()
-	{
-		int random;
-		for(int i = 0; i < mineCount; i++) {
-			random = (int)(Math.random() * (boardSizeX * boardSizeY + 1));
-			while(mineLocations.contains(random)) {
-				random = (int)(Math.random() * (boardSizeX * boardSizeY + 1));
-			}
-			System.out.println(random);
-			mineLocations.add(random);
-		}
-	}
+
 
 	public int countNearbyMines(int x, int y)
 	{
@@ -177,35 +241,99 @@ public class Map extends JFrame implements ActionListener {
 		return count;
 	}
 
-	public void actionPerformed(ActionEvent e)
-	{
-		for (JButton btn : buttons) {
-			if (e.getSource() == btn) {
-				if(btn.getName().equals("Empty")) {
-					int nearbyMines = countNearbyMines(btn.getX()/30, btn.getY()/30);
-					if(nearbyMines > 0) {
-						JLabel mineText = new JLabel(Integer.toString(nearbyMines));
-						mineText.setBounds(btn.getBounds());
-						mineText.setVisible(true);
-						frame.add(mineText);
-					}
-					btn.setVisible(false);
-					break;
-				}
-				else {
-					for (JButton button : buttons) {
-						if(button.getName().equals("Mine")) {
-							button.setVisible(false);
-						}
-					}
-					break;
-				}
-			}
-		}
-	}
+
 
 	public void rightClick(int x, int y) {
 		// TODO - implement Map.rightClick
 		throw new UnsupportedOperationException();
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1)
+		{
+			System.out.println("no");
+			for (JButton btn : buttons) {
+				if (e.getSource() == btn) {
+					if(btn.getName().equals("Empty")) {
+						int nearbyMines = countNearbyMines(btn.getX()/30, btn.getY()/30);
+						if(nearbyMines > 0) {
+							JLabel mineText = new JLabel(Integer.toString(nearbyMines));
+							mineText.setBounds(btn.getBounds());
+							mineText.setVisible(true);
+							frame.add(mineText);
+
+						}
+						//else{
+						//open empties}
+						btn.setVisible(false);
+						break;
+					}
+
+					else {
+						for (JButton button : buttons) {
+							if(button.getName().equals("Mine")) {
+								for (JButton button1 : buttons){
+									if(button1.getName().equals("Mine")) {
+										button1.setVisible(false);}
+								}
+								JOptionPane.showMessageDialog(null, "R.I.P. YOU HAVE PRESSED THE BOMB", "Game Over", JOptionPane.CLOSED_OPTION);
+
+								frame.setVisible(false);
+								break;
+							/*endframe.setVisible(true);
+							endframe.setLayout(null);
+							endframe.setLocation(500,500);*/
+
+								//todo endgame
+							}
+						}
+						break;
+					}
+				}
+			}
+
+
+		}
+		if (e.getButton() == MouseEvent.BUTTON3)
+		{
+			System.out.println("yes");
+			for (JButton btn : buttons) {
+				if (e.getSource() == btn) {
+					if (track == 0)
+					{ track = 1;
+							btn.setText("F");
+					bombCount--;}
+					else if (track == 1)
+					{ track = 0;
+						btn.setText("");
+						bombCount++;
+
+					}
+				}
+
+				}
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
 	}
 }
